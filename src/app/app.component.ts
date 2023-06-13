1c33866d0bf33bb7da97647921865b6990ade01a
alexlamb10 <99216731+alexlamb10@users.noreply.github.com>
2023-06-13 11:21:55 -0600

    added necessary methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { map, startWith } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -19,5 +20,28 @@ export class AppComponent {
     return this.todosFormArray.controls as FormGroup[];
   }
 
+  public formUpdates$ = this.form.valueChanges.pipe(
+    startWith(this.form.value),
+    map((value) => {
+      return {
+        ...value,
+        valid: this.form.valid,
+      };
+    })
+  );
+
   constructor(private _fb: FormBuilder) {}
+
+  addTodoFormGroup() {
+    const newTodoGroup = this._fb.group({
+      title: ['', [Validators.required]],
+      complete: false,
+    });
+
+    this.todosFormArray.push(newTodoGroup);
+  }
+
+  removeFormGroup(index: number) {
+    this.todosFormArray.removeAt(index);
+  }
 }
